18135c308f6921ccc3f48c80843e2c11f8df70e1
Hameet <hameet.singh@hotmail.com>
2018-12-19 09:53:14 +1300

    Fetched data now rendering

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 import ApolloClient from 'apollo-boost'
 import { ApolloProvider } from 'react-apollo'
 
+import Constants from './Constants'
+
 const client = new ApolloClient({
-  url: 'https://vm8mjvrnv3.lp.gql.zone/graphql'
+  uri: 'https://vm8mjvrnv3.lp.gql.zone/graphql'
 })
 
 const App = () => (
   <ApolloProvider client={client}>
     <div>
-      <h2>My Apollo React Application</h2>
+      <Constants />
     </div>
   </ApolloProvider>
 )
